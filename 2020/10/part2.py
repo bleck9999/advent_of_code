3127b3e9c8f0034beeed9f1bf8dc8f466792d99a
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-12-10 20:15:05 +0000

    2020 day 10

--- 2020/10/part2.py
+++ 2020/10/part2.py
@@ -1,3 +1,39 @@
+
+
+
+
+
+
+
+# hey
+
+# i couldnt do day 10
+
+
+
+
+# the solution was made by someone much more competent than me
+# posted by tboschi on github
+# https://github.com/tboschi/advent-of-code-2020/tree/master/day10/py
+# everything after line 48 is not mine
+# have a nice day
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 adapters = []
 with open("input", "r") as f:
     for line in f.readlines():
@@ -7,8 +43,17 @@ adapters.sort()
 adapters.insert(0, 0)
 
 
-# i give up
-# i couldnt do it
-# maybe ill come back to this one another time
-# but for now
-# here lies a stub
+# frii theft
+# i have no idea how this works
+arrangements = [1]
+for i in range(1, len(adapters)):
+    arrange = arrangements[i-1]
+    j = i - 2
+    while j >= 0 and adapters[i] - adapters[j] <= 3:
+        arrange += arrangements[j];
+        j -= 1
+
+    arrangements.append(arrange);
+
+
+print(f"There are {arrangements[-1]} valid arrangements")
