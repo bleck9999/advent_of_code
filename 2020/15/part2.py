47bef78a81ad1b8915f2b0d33e84952f6d473c9d
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-12-16 19:36:59 +0000

    it now has a progress bar so you can watch the O(n²) in real time

--- 2020/15/part2.py
+++ 2020/15/part2.py
@@ -1,26 +1,23 @@
-# start = []
-# for x in open("input", "r").readlines():
-#     x = x.strip().split(sep=',')
-#     for y in x:
-#         start.append(int(y.strip()))
-#
-#
-#
-# def playgaem(start, until):
-#     seen = start[:]
-#     while True:
-#         last_seen = seen[-1]
-#         if last_seen in seen[:-1]:
-#             last = len(seen) - seen.index(last_seen)
-#             sndlast = len(seen) - seen.index(last_seen )
-#             seen.insert(len(seen), last-sndlast)
-#         else:
-#             seen.append(0)
-#         if len(seen) == until:
-#             return seen[until-1]
-#
-#
-# # ok nevermind this takes a day to run
-# print(playgaem(start, 30000000))
+from tqdm import tqdm
+start = []
+for x in open("input", "r").readlines():
+    x = x.strip().split(sep=',')
+    for y in x:
+        start.append(int(y.strip()))
 
-# aa
+
+def playgaem(start, until):
+    seen = start[::-1]
+    for _ in tqdm(range(until)):
+        last_seen = seen[0]
+        if last_seen in seen[1:]:
+            last = seen.index(last_seen)
+            sndlast = seen.index(last_seen, 1)
+            seen.insert(0, sndlast-last)
+        else:
+            seen.insert(0, 0)
+
+    return seen[until-1]
+
+
+print(playgaem(start, 30000000))
